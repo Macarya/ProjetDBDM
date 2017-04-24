3188aa396d7d5f1f872fce70787f77057a6e3c4c
XBdM <xavier.badin@gmail.com>
2017-04-24 15:01:40 +0200

    Deuxième conversion matrice liste to matrice
    Correction conversion matrice liste to matrice

--- matlist2mat.py
+++ matlist2mat.py
@@ -5,5 +5,8 @@ Created on Mon Apr 24 14:33:24 2017
 @author: Xavier
 """
 
+M = [[[1,2],[],[1,5,6,7]],[[1,19,0.122],[],[1,5,6,7]],[[1,2],[],[1,5,6,7]]]
+
 def mtxl2m(M):
-    return [[-1 for l in t if not len(l) else sum(l)/len(l)] for t in M]
+    return [[-1 if not len(l) else sum(l)/len(l) for l in t] for t in M]
+
