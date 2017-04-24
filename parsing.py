d7779fa966753bb00de4cc9f95d3997de05bfda1
Makarya <ygaziello@gmail.com>
2017-04-24 15:55:50 +0200

    Parsing

--- parsing.py
+++ parsing.py
@@ -9,24 +9,22 @@ def parse(NomFichier='out.txt'):
     with open(NomFichier,'r') as fichier:
         texte=fichier.read()
         
-    texte=texte.split(',')
+    texte=texte.split(',')[:-1]
     Resultat=[]
-    for j in range(123):
-        Resultat.append([ [] for i in range(123)])
-    Duree=[ [] for i in range(123)]
+    for j in range(248):
+        Resultat.append([ [] for i in range(248)])
+    Duree=[ [] for i in range(248)]
     
     pkey,ptime=int(texte[0]),float(texte[2])
     Duree[pkey].append(ptime)
     for n in range(3,len(texte),3):
-        if texte[n]=='\n':
-            break
         key,time=int(texte[n]),float(texte[n+2])
         if texte[n+1]=='+':
             Duree[key].append(-time)
             Resultat[pkey][key].append(time-ptime)
+            pkey,ptime=key,time
         else:
             Duree[key][-1]+=time
-            pkey,ptime=key,time
     return(Duree,Resultat)
     
     
