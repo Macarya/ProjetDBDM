8c422772236235513b581b77f4fcb3acb8ddd85e
XBdM <xavier.badin@gmail.com>
2017-04-24 16:03:57 +0200

    premiere version finale
    Première version

--- matlist2mat.py
+++ matlist2mat.py
@@ -5,7 +5,37 @@ Created on Mon Apr 24 14:33:24 2017
 @author: Xavier
 """
 
-M = [[[1,2],[],[1,5,6,7]],[[1,19,0.122],[],[1,5,6,7]],[[1,2],[],[1,5,6,7]]]
+def aff(res):
+    for i in range(len(res)):
+        for j in range(len(res[i])):
+            if res[i][j]!=[]:
+                print(i,j)
+
+
+def parse(NomFichier='out.txt'):
+    with open(NomFichier,'r') as fichier:
+        texte=fichier.read()
+        
+    texte=texte.split(',')[:-1]
+    Resultat=[]
+    for j in range(248):
+        Resultat.append([ [] for i in range(248)])
+    Duree=[ [] for i in range(248)]
+    
+    pkey,ptime=int(texte[0]),float(texte[2])
+    Duree[pkey].append(ptime)
+    for n in range(3,len(texte),3):
+        key,time=int(texte[n]),float(texte[n+2])
+        if texte[n+1]=='+':
+            Duree[key].append(-time)
+            Resultat[pkey][key].append(time-ptime)
+            pkey,ptime=key,time
+        else:
+            Duree[key][-1]+=time
+    return(Duree,Resultat)
+    
+    
+
 
 def mtxl2m(M):
     return [[-1 if not len(l) else sum(l)/len(l) for l in t] for t in M]
