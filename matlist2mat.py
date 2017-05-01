d9d6e0ecf25b9caaf1a76117696b723f21116f30
XBdM <xavier.badin@gmail.com>
2017-05-01 16:26:13 +0200

    Moyenne géométrique
    rajout moyenne géométrique pour comparaison matrice, moyenne sans
    quartile pour création matrice

--- matlist2mat.py
+++ matlist2mat.py
@@ -34,13 +34,22 @@ def parse(NomFichier='out.txt'):
             Duree[key][-1]+=time
     return(Duree,Resultat)
     
+def moyenne(l):
+    if (not len(l)):
+        return -1
+    return sum(l)/len(l)
+
+def moyenne_sans_quartil(l,q=4):
+    n = len(l)
+    if(not n):
+        return -1
+    l_tempo = sorted(l)
+    return moyenne(l_tempo[(n//q):min(n,(q-1)*((n+q)//q))])
     
+def mtxl2m(M,f=moyenne):
+    return [[f(l) for l in t] for t in M]
 
-
-def mtxl2m(M):
-    return [[-1 if not len(l) else sum(l)/len(l) for l in t] for t in M]
-
-def dist2m(M1,M2):
+def dist2m_moya(M1,M2):
     a = 0;
     k = 0;
     for i in range(len(M1)):
@@ -51,3 +60,33 @@ def dist2m(M1,M2):
     if k > 0:
         return a/k
     return -1
+
+def dist2m_moyg(M1,M2):
+    a = 0;
+    k = 0;
+    for i in range(len(M1)):
+        for j in range(len(M1[i])):
+            if (M1[i][j]>0 and M2[i][j]>0):
+                if (M1[i][j]<M2[i][j]):
+                    a+=M1[i][j]/M2[i][j]
+                else :
+                    a+=M2[i][j]/M1[i][j]
+                k+=1
+    if k > 0:
+        return a/k
+    return -1
+
+_,Mxa = parse('out.txt')
+_,Mra = parse('out(1).txt')
+_,Mxt = parse('out(2).txt')
+_,Mrt = parse('out(3).txt')
+
+Mxa1 = mtxl2m(Mxa,moyenne_sans_quartil)
+Mra1 = mtxl2m(Mra,moyenne_sans_quartil)
+Mxt1 = mtxl2m(Mxt,moyenne_sans_quartil)
+Mrt1 = mtxl2m(Mrt,moyenne_sans_quartil)
+
+print(dist2m_moyg(Mxa1,Mxt1))
+print(dist2m_moyg(Mxa1,Mrt1))
+print(dist2m_moyg(Mra1,Mxt1))
+print(dist2m_moyg(Mra1,Mrt1))
