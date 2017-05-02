ffd0c3e0532a3ade09e69b5e407ab6bbe2af8f3a
XBdM <xavier.badin@gmail.com>
2017-05-02 11:24:10 +0200

    Rajout appui touche
    On rajoute la durée d'appui sur une touche avec tests correpondant

--- matlist2mat.py
+++ matlist2mat.py
@@ -39,7 +39,7 @@ def moyenne(l):
         return -1
     return sum(l)/len(l)
 
-def moyenne_sans_quartil(l,q=6):
+def moyenne_sans_quartil(l,q=4):
     n = len(l)
     if(not n):
         return -1
@@ -48,6 +48,10 @@ def moyenne_sans_quartil(l,q=6):
     
 def mtxl2m(M,f=moyenne):
     return [[f(l) for l in t] for t in M]
+
+def ll2m(L,f=moyenne):
+    return [f(l) for l in L]
+
 
 def dist2m_moya(M1,M2):
     a = 0;
@@ -76,12 +80,38 @@ def dist2m_moyg(M1,M2):
         return a/k
     return -1
 
-_,Mxa = parse('xavier_dicté.txt')
-_,Mra = parse('redouane_dicté.txt')
-_,Mxt = parse('xavier_test.txt')
-_,Mrt = parse('redouane_test.txt')
-_,Mxl = parse('xavier_lorem_ipsum.txt')
-_,Mxc = parse('xavier_copie_daudet_derniere_classe.txt')
+
+def dist2l_moya(L1,L2):
+    a = 0;
+    k = 0;
+    for i in range(len(L1)):
+        if (L1[i]>0 and L2[i]>0):
+            a+=abs(L1[i]-L2[i])
+            k+=1
+    if k > 0:
+        return a/k
+    return -1
+
+def dist2l_moyg(L1,L2):
+    a = 0;
+    k = 0;
+    for i in range(len(L1)):
+        if (L1[i]>0 and L2[i]>0):
+            if (L1[i]>L2[i]):
+                a+=L1[i]/L2[i]
+            else :
+                a+=L2[i]/L1[i]
+            k+=1
+    if k > 0:
+        return a/k
+    return -1
+
+Lxa,Mxa = parse('xavier_dicté.txt')
+Lra,Mra = parse('redouane_dicté.txt')
+Lxt,Mxt = parse('xavier_test.txt')
+Lrt,Mrt = parse('redouane_test.txt')
+Lxl,Mxl = parse('xavier_lorem_ipsum.txt')
+Lxc,Mxc = parse('xavier_copie_daudet_derniere_classe.txt')
 
 Mxa1 = mtxl2m(Mxa,moyenne_sans_quartil)
 Mra1 = mtxl2m(Mra,moyenne_sans_quartil)
@@ -89,6 +119,26 @@ Mxt1 = mtxl2m(Mxt,moyenne_sans_quartil)
 Mrt1 = mtxl2m(Mrt,moyenne_sans_quartil)
 Mxl1 = mtxl2m(Mxl,moyenne_sans_quartil)
 Mxc1 = mtxl2m(Mxc,moyenne_sans_quartil)
+
+Lxa1 = ll2m(Lxa,moyenne_sans_quartil)
+Lra1 = ll2m(Lra,moyenne_sans_quartil)
+Lxt1 = ll2m(Lxt,moyenne_sans_quartil)
+Lrt1 = ll2m(Lrt,moyenne_sans_quartil)
+Lxl1 = ll2m(Lxl,moyenne_sans_quartil)
+Lxc1 = ll2m(Lxc,moyenne_sans_quartil)
+
+Lxa1[16] = -1
+Lra1[16] = -1
+Lxt1[16] = -1
+Lrt1[16] = -1
+Lxl1[16] = -1
+Lxc1[16] = -1
+Lxa1[9] = -1
+Lra1[9] = -1
+Lxt1[9] = -1
+Lrt1[9] = -1
+Lxl1[9] = -1
+Lxc1[9] = -1
 
 #print(dist2m_moyg(Mxa1,Mra1))
 print("juste")
@@ -129,3 +179,45 @@ print(dist2m_moya(Mxl1,Mxc1))
 print("faux")
 print(dist2m_moya(Mxc1,Mrt1))
 print(dist2m_moya(Mxc1,Mra1))
+
+print("\n touche \n")
+
+#print(dist2l_moyg(Lxa1,Lra1))
+print("juste")
+print(dist2l_moyg(Lxa1,Lxt1))
+print("faux")
+print(dist2l_moyg(Lxa1,Lrt1))
+print(dist2l_moyg(Lra1,Lxt1))
+#print(dist2l_moyg(Lra1,Lrt1))
+#print(dist2l_moyg(Lxa1,Lxl1))
+#print(dist2l_moyg(Lra1,Lxl1))
+#print(dist2l_moyg(Lxt1,Lxl1))
+#print(dist2l_moyg(Lrt1,Lxl1))
+print("juste")
+print(dist2l_moyg(Lxa1,Lxc1))
+print(dist2l_moyg(Lxt1,Lxc1))
+print(dist2l_moyg(Lxl1,Lxc1))
+print("faux")
+print(dist2l_moyg(Lxc1,Lrt1))
+print(dist2l_moyg(Lxc1,Lra1))
+
+print("\n ari \n")
+
+#print(dist2l_moya(Lxa1,Lra1))
+print("juste")
+print(dist2l_moya(Lxa1,Lxt1))
+print("faux")
+print(dist2l_moya(Lxa1,Lrt1))
+print(dist2l_moya(Lra1,Lxt1))
+#print(dist2l_moya(Lra1,Lrt1))
+#print(dist2l_moya(Lxa1,Lxl1))
+#print(dist2l_moya(Lra1,Lxl1))
+#print(dist2l_moya(Lxt1,Lxl1))
+#print(dist2l_moya(Lrt1,Lxl1))
+print("juste")
+print(dist2l_moya(Lxa1,Lxc1))
+print(dist2l_moya(Lxt1,Lxc1))
+print(dist2l_moya(Lxl1,Lxc1))
+print("faux")
+print(dist2l_moya(Lxc1,Lrt1))
+print(dist2l_moya(Lxc1,Lra1))
