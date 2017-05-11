f603606989d435005bf1aba39c1816a49ce5eea2
Makarya <ygaziello@gmail.com>
2017-05-11 13:23:53 +0200

    riej

--- distance.py
+++ distance.py
@@ -1,16 +1,16 @@
-from matlist2mat import mtxl2m
+from matlist2mat import mtxl2m,dist2m_moyap
 from math import sqrt
 
 #Training matrice de paire (moyenne, ecart_type)
 #Sample matrice de liste de temps
-def distance(Training, Sample):
+def distance(Training, Sample, delta):
 	score = 0
 	for i in range(len(Sample)):
 		for j in range(len(Sample)):
-			mean, std = Training[i][j]
+			mean, std, long = Training[i][j]
 			std = sqrt(std)
-			min = mean - 1.5*std
-			max = mean + 1.5*std
+			min = mean - delta*std
+			max = mean + delta*std
 			for x in Sample[i][j]:
 				if (x > min) and (x < max):
 					score += 1
@@ -23,21 +23,22 @@ class Classifieur:
 	def addTraining(self, className, matrix):
 		self.trainings[className] = mtxl2m(matrix)
 
-	def predict(self, matrix):
+	def predict(self, matrix, delta=1.2):
 		maxi = -1
 		best = ""
 		for clas, clasMat in self.trainings.items():
-			s = distance(clasMat, matrix)
+			s = distance(clasMat, matrix, delta)
 			if s > maxi:
 				maxi = s
 				best = clas
 		return best
-    
-    def predictmoyap(self, matrix):
+
+	def predictmoyap(self, matrix):
 		maxi = 1000000
 		best = ""
 		for clas, clasMat in self.trainings.items():
 			s = dist2m_moyap(clasMat, matrix)
+			print(clas,s)
 			if s < maxi:
 				maxi = s
 				best = clas
--- scripttest.py
+++ scripttest.py
@@ -6,16 +6,29 @@ from distance import *
 from parsing import *
 
 classifieur=Classifieur()
-Lname=['lois']
+Lname=['lois','Yannis','Xavier','Angele','hugo','Emile','red']
 
 for name in Lname:
-    dur1,res1=parse('Records/'+name+'-Phrase1.txt')
-    dur2,res2=parse('Records/'+name+'-Phrase2.txt')
-    dur3,res3=parse('Records/'+name+'-Phrase3.txt')
+    dur1,res1=parse('Records/'+name+'-phrase1.txt')
+    dur2,res2=parse('Records/'+name+'-phrase2.txt')
+    dur3,res3=parse('Records/'+name+'-phrase3.txt')
     res,dur=concatRes(res1),concatDuree(dur1)
     classifieur.addTraining(name,res)
 
-for name in Lname:
+"""for delta in [1,1.2,1.5,1.8,2,2.2,3]:
+    score=0
+    for name in Lname:
+        for i in range(4,7):
+            dur,res=parse('Records/'+name+'-phrase'+str(i)+'.txt')
+            nameres=classifieur.predict(res,delta)
+            if nameres==name:
+                score+=1
+    print(score)"""
+        
+"""for name in Lname:
+    print(name+':')
     for i in range(4,7):
-        dur,res=parse('Records/'+name+'-Phrase'+str(i)+'.txt')
-        classifieur.predict(res)
+        dur,res=parse('Records/'+name+'-phrase'+str(i)+'.txt')
+        print(classifieur.predictmoyap(res))
+        print('\n')
+    print('\n')"""
